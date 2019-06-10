687ce8e09b5e950db07be08cf4f0c3f775fdee98
github username <github email address>
2019-06-10 16:35:43 +0100

    completed task

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,37 @@
 class Gym
-  attr_reader :name
+
+  attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    # Get a list of all gyms
+    @@all
+  end
+
+  def membership
+    # Get a list of all memberships at a specific gym
+    Membership.all.select{|m| m.gym == self }
+  end
+
+  def lifter_list
+    # Get a list of all the lifters that have a membership to a specific gym
+    membership.collect{|m| m.lifter}.uniq
+  end
+
+  def lifter_names
+    # Get a list of the names of all lifters that have a membership to that gym
+    lifter_list.collect{|l| l.name}
+  end
+
+  def gym_total
+    # Get the combined lift total of every lifter has a membership to that gym
+    membership.sum{|m| m.lifter.lift_total}
+  end
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,31 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.average_lift_total  
+    @@all.sum{|lifter| lifter.lift_total} / @@all.length
+  end
+
+  def sign_up(membership_cost, gym)
+    Membership.new(membership_cost, self, gym)
+  end
+
+  def total_membership_cost
+     Membership.all.select.sum{|m| m.cost if m.lifter == self}
+  end
+
 end
+
+
+
